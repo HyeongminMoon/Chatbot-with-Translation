476e34631d9abe9806f97ec34728b294701637bc
Hyeongmin Moon <mohomin123@gmail.com>
2023-04-28 15:38:19 +0900

    fix error, add langdetect

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 from datetime import datetime
 import string
 import random
+from langdetect import detect
 
 app = FastAPI()
 result_save_dir = "test_results"
@@ -21,6 +22,10 @@ class DefaultTS():
 
 class PromptData(BaseModel):
     text: str
+
+
+def check_lang(text: str):
+    return detect(text)
 
 
 def save_result(result: dict):
@@ -56,19 +61,14 @@ def predict(data: PromptData, ts_tool: str):
     if ts_tool not in ts_pool:
         return {"error": f"There is no matched name of translator. available:{ts_pool}"}
     try:
-        if data.text.encode().isalpha():
+        ori_lang = check_lang(data.text)
+        if ori_lang == 'en':
             result = instruct_pipeline(data)[0]
             # result["generated_text"]
-            result["detected_lang"] = 'en'
         else:
             q_text = data.text
-            info = ts.translate_text(
-                q_text, translator=DefaultTS.ts_tool, if_use_preacceleration=True,
-                to_language='en', is_detail_result=True, timeout=10
-            )
-            ori_lang = info.get("srcLangTdype")
             ts_text = ts.translate_text(
-                q_text, translator=ts_tool, if_use_preacceleration=True,
+                q_text, translator=DefaultTS.ts_tool, if_use_preacceleration=True,
                 to_language='en', timeout=10
             )
             result = instruct_pipeline({"text": ts_text})[0]
@@ -83,6 +83,8 @@ def predict(data: PromptData, ts_tool: str):
             result["back_translator"] = ts_tool
             result["translated_text"] = ts_text
             result["detected_lang"] = ori_lang
+
+        result["detected_lang"] = ori_lang
         result["generate_model"] = generate_model  
         result["original_text"] = data.text
         save_result(result)
@@ -98,18 +100,16 @@ def predict(data: PromptData):
     if not STARTUP:
         return {"error": "Sever is starting. It takes ~2min for optimization."}
     try:
-        if data.text.encode().isalpha():
+        ori_lang = check_lang(data.text)
+        if ori_lang == 'en':
             result = instruct_pipeline(data)[0]
             # result["generated_text"]
-            result["detected_lang"] = 'en'
         else:
             q_text = data.text
-            info = ts.translate_text(
+            ts_text = ts.translate_text(
                 q_text, translator=DefaultTS.ts_tool, if_use_preacceleration=True,
-                to_language='en', is_detail_result=True, timeout=10
+                to_language='en', timeout=10
             )
-            ori_lang = info.get("srcLangTdype")
-            ts_text = info.get("translatedText")
             result = instruct_pipeline({"text": ts_text})[0]
             gen_text = result["generated_text"]
             ts_gen_text = ts.translate_text(
@@ -122,6 +122,8 @@ def predict(data: PromptData):
             result["back_translator"] = ts_tool
             result["translated_text"] = ts_text
             result["detected_lang"] = ori_lang
+
+        result["detected_lang"] = ori_lang
         result["generate_model"] = generate_model  
         result["original_text"] = data.text
         save_result(result)
@@ -136,15 +138,22 @@ def translate(data: PromptData, ts_tool: str):
     if not STARTUP:
         return {"error": "Sever is starting. It takes ~2min for optimization."}
     try:
-        result = {}
-        q_text = data.text
-        translated_text = ts.translate_text(
-            q_text, translator=ts_tool, if_use_preacceleration=True
-        )
+        ori_lang = check_lang(data.text)
+        if ori_lang == 'en':
+            pass
+        else:
+            result = {}
+            q_text = data.text
+            translated_text = ts.translate_text(
+                q_text, translator=ts_tool, if_use_preacceleration=True
+            )
+            result["original_text"] = data.text
+            result["translator"] = ts_tool
+            result["translated_text"] = translated_text
+            save_result(result)
+
+        result["detected_lang"] = 'en'
         result["original_text"] = data.text
-        result["translator"] = ts_tool
-        result["translated_text"] = translated_text
-        save_result(result)
         return result
     except Exception as e:
         return {"error": repr(e)}
